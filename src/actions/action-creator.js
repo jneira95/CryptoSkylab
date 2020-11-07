55093da86b58fc37c3136c5d6ca32ff1c40fdef9
Jorge <jorgengarriga@gmail.com>
2020-11-07 20:47:46 +0100

    Add loadCoinList function

--- src/actions/action-creator.js
+++ src/actions/action-creator.js
@@ -2,10 +2,10 @@ import axios from 'axios';
 import actionTypes from '../actions/action-types';
 import dispatcher from '../dispatcher/disptacher';
 
-export async function loadCoinsList() {
+export async function loadCoinsList(per_page = 25, page = 1) {
 	try {
 		const coinsList = await axios.request(
-			'https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&order=market_cap_desc&per_page=50&page=1&sparkline=true&price_change_percentage=1h%2C24h'
+			`https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&order=market_cap_desc&per_page=${per_page}&page=${page}&sparkline=true&price_change_percentage=1h%2C24h`
 		);
 		console.log(coinsList);
 		dispatcher.dispatch({
