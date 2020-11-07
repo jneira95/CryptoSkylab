4c3371cfbcfcfd97a2d2c2e22d0debd515e28f08
Jorge <jorgengarriga@gmail.com>
2020-11-07 20:48:26 +0100

    Add main list of cryptoCurrencies

--- src/components/CryptoListComponent/CryptoList.jsx
+++ src/components/CryptoListComponent/CryptoList.jsx
@@ -4,18 +4,18 @@ import { loadCoinsList } from '../../actions/action-creator';
 import CryptoListTableInfo from './CryptoListTableInfo';
 function CryptoList() {
 	const [cryptoList, setCryptoList] = useState(null);
-
+	const [currentPage, setCurrentPage] = useState(1);
+	const [currentItemsPerPage] = useState(25);
 	function handleChange() {
 		setCryptoList(cryptoStore.getCryptoList);
 	}
-
 	useEffect(() => {
 		cryptoStore.addEventListener(handleChange);
 		!cryptoList && loadCoinsList();
 		return () => {
 			cryptoStore.removeEventListener(handleChange);
 		};
-	});
+	}, [cryptoList, currentPage, currentItemsPerPage]);
 
 	return (
 		<>
@@ -45,6 +45,42 @@ function CryptoList() {
 							})}
 						</tbody>
 					</table>
+					<button
+						type="button"
+						onClick={() => {
+							loadCoinsList(currentItemsPerPage, currentPage + 1);
+							setCurrentPage(currentPage + 1);
+						}}
+					>
+						next
+					</button>
+					<button
+						type="button"
+						onClick={() => {
+							console.log(currentPage);
+							if (currentPage > 1) {
+								loadCoinsList(currentItemsPerPage, currentPage - 1);
+								setCurrentPage(currentPage - 1);
+							}
+							console.log(`--->${currentPage}`);
+						}}
+					>
+						previous
+					</button>
+					<form>
+						<select
+							name="items"
+							id="itemstoshow"
+							onChange={(event) => {
+								loadCoinsList(event.target.value, currentPage);
+								console.log(event.target.value);
+							}}
+						>
+							<option value={25}>25</option>
+							<option value={50}>50</option>
+							<option value={100}>100</option>
+						</select>
+					</form>
 				</section>
 			)}
 		</>
